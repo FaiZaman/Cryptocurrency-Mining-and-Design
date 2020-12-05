9a34980322984a1058d89ab271d4f75fe892eacc
Faiz <faiz-uz.zaman@dur.ac.uk>
2020-12-05 15:37:06 +0000

    Computed hit value and checked for it being less than target

--- pos_mining_puzzle.py
+++ pos_mining_puzzle.py
@@ -1,5 +1,6 @@
 import hashlib
 import ecdsa
+import time
 
 # An the previous block header - do not change any fields
 previous_block_header = {
@@ -34,12 +35,46 @@ effective_balance = 66
 sk = ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1)
 vk = sk.verifying_key
 
-# get signature for Hello World
-signature = sk.sign(b"Hello World")
+# signs the message 'Hello World'
+def sign_hello_world():
 
-# convert verifying key and signature to hex
-vk_hex = vk.to_string().hex()
-signature_hex = signature.hex()
+	# get signature for Hello World
+	signature = sk.sign(b"Hello World")
 
-print(vk_hex)
-print(signature_hex)
+	# convert verifying key and signature to hex
+	vk_hex = vk.to_string().hex()
+	signature_hex = signature.hex()
+
+	print(vk_hex)
+	print(signature_hex)
+
+
+# calculate new target value T
+# T = Tb * S * Be
+# Be = effective_balance
+
+base_target = previous_block_header['baseTarget']   # Tb
+
+time_last_block_created = previous_block_header['timestamp']
+current_time = time.time()
+time_since_last_block = current_time - time_last_block_created	# S
+print(time_last_block_created, time_since_last_block)
+
+new_target = base_target * time_since_last_block * effective_balance
+
+# compute hit value
+hit_value = float('inf')
+prev_gen_sig = previous_block_header['generationSignature']
+
+print(new_target)
+while hit_value > new_target:
+
+	mine = sk.sign(bytes.fromhex(prev_gen_sig))
+	total_hit_value = hashlib.sha256(hashlib.sha256(mine).digest()).hexdigest()
+
+	# take first 8 bytes and compare to target
+	real_hit_value = total_hit_value[:16]
+	hit_value = int(real_hit_value, 16)
+	print(hit_value)
+
+print(hit_value)
