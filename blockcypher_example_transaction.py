cea69183c1bf269bdcce0aa8cd6124d0730b0192
Faiz <faiz-uz.zaman@dur.ac.uk>
2020-12-23 17:00:29 +0400

    Finished transaction writing user ID to blockchain

--- blockcypher_example_transaction.py
+++ blockcypher_example_transaction.py
@@ -10,10 +10,8 @@ import json
 public_key = '03f2fb06f61d8dfe3451ee55a1e86578665f4d1548465d207f301865fa1be2d5df'
 private_key = '6cd46f66cc9b262adc2aa6d60666aed8271bbcd1e2b4393dbadab3fd50c18ec9'
 
+# sends 'val' satoshis to address
 def value_transaction(val):
-
-    # Transaction ID for getting 0.001 BTC from testnet:
-    # 25fe94e691ece5a4a04b90fa0a319a946e0a4d30b483b4e195342e67e23e68a2"
 
     inputs = [{'address': 'mhihAtVZhNT13zzWA2AUyQFYtUyaunDKSp'}]
     outputs = [{'address': 'mpamtqLA66JFVSQNDaPHZ5xMiCz6T2MeNn', 'value': val}]
@@ -41,18 +39,22 @@ def value_transaction(val):
     # https://sochain.com/tx/BTCTEST/8fbfaf8213746c3beb5e2592250389649500e19e325f6603f02f515d8655e5eb
 
 
-# value_transaction(100) - runs 100 satoshis transaction
-
+# writes a user ID to the blockchain
 def write_ID_transaction(ID):
 
     inputs = [{'address': 'mhihAtVZhNT13zzWA2AUyQFYtUyaunDKSp'}]
 
     # hex encoding of a suitable script to create a proof of burn transaction with ID on it
     hex_ID = str.encode(ID).hex()    # hex version of student ID
-    script = "6a" + hex_ID + "76a9" + public_key + "88ac"
 
+    # 6a - opcode for OP_RETURN
+    # 06 - length of metadata (user ID) in bytes
+    script = "6a06" + hex_ID
+
+    # assigning script to output
     outputs = [{'value': 0, 'script_type': "null-data", 'script': script}]
 
+    # create transaction as in previous task
     unsigned_tx = blockcypher.create_unsigned_tx(inputs=inputs, outputs=outputs, 
                         coin_symbol='btc-testnet', api_key='6e74ffe16f4a4e3fbd13acd5a3d01014')
 
@@ -67,5 +69,8 @@ def write_ID_transaction(ID):
     blockcypher.broadcast_signed_transaction(unsigned_tx=unsigned_tx, signatures=tx_signatures,\
         pubkeys=public_keys, coin_symbol='btc-testnet', api_key='6e74ffe16f4a4e3fbd13acd5a3d01014')
 
+    # Transaction ID: cb19c2dc3e0eb274c825d082fff4d09f1d4c8c66709f2dd0f74fd157c1757562
+    # https://sochain.com/tx/BTCTEST/8fbfaf8213746c3beb5e2592250389649500e19e325f6603f02f515d8655e5eb
 
-write_ID_transaction('psgg66')
+# value_transaction(100) - runs 100 satoshis transaction
+write_ID_transaction('psgg66')  # - writes user ID to blockchain
